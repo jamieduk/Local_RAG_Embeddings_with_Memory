b7787d156da169fd6ed510c6028a03144b742863
Jay <jamied_uk@hotmail.com>
2024-05-27 14:29:01 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-echo "Starting RAG V3"
+
+
+
+echo "Setting up pre-requisites..."
+if [ -d "myenv" ]; then
+    echo "Virtual environment 'myenv' already exists."
+    source myenv/bin/activate
+else
+    echo "Creating virtual environment 'myenv'..."
+    python3 -m venv myenv 
+    source myenv/bin/activate
+    echo "Virtual environment 'myenv' created."
+fi
+
+
+
+echo "Starting RAG V7"
 python localragV7.py
